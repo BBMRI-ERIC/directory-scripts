0ec097b325b8c038a462434f66206517bf26d0c4
Petr Holub <hopet@ics.muni.cz>
2018-07-13 19:15:41 +0200

    updates

--- checks/ContactFields.py
+++ checks/ContactFields.py
@@ -1,5 +1,8 @@
 import re
+
 from yapsy.IPlugin import IPlugin
+from validate_email import validate_email
+
 from customwarnings import WarningLevel,Warning
 
 class CheckContactFields(IPlugin):
@@ -12,4 +15,14 @@ class CheckContactFields(IPlugin):
 			if(not 'last_name' in contact or re.search('^\s*$', contact['last_name'])):
 				warning = Warning("", dir.getContactNN(contact['id']), WarningLevel.WARNING, "Missing last name for contact " + contact['id'])
 				warnings.append(warning)
+			if(not 'email' in contact or re.search('^\s*$', contact['email'])):
+				warning = Warning("", dir.getContactNN(contact['id']), WarningLevel.ERROR, "Missing email for contact " + contact['id'])
+				warnings.append(warning)
+			elif(not validate_email(contact['email'])):
+				warning = Warning("", dir.getContactNN(contact['id']), WarningLevel.WARNING, "Email for contact " + contact['id'] + " is invalid " + contact['email'])
+				warnings.append(warning)
+			# This is pretty dramatic test and should be used sparingly
+			#if(not validate_email(contact['email'],verify=True)):
+				#warning = Warning("", dir.getContactNN(contact['id']), WarningLevel.WARNING, "Email for contact " + contact['id'] + " is invalid")
+				#warnings.append(warning)
 		return warnings
