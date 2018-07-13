059ad1f0d31e4a0cd7c7eedaec82b07469f7b414
Petr Holub <hopet@ics.muni.cz>
2018-07-13 20:36:55 +0200

    updates

---
+++ checks/BiobankGeo.py
@@ -0,0 +1,36 @@
+import re
+
+from yapsy.IPlugin import IPlugin
+from customwarnings import WarningLevel,Warning
+
+from geopy.geocoders import Nominatim
+
+class CheckBiobankGeo(IPlugin):
+	def check(self, dir):
+		geolocator = Nominatim()
+		warnings = []
+		# This is to be enabled for real runs.
+		geoCodingEnabled = False
+		for biobank in dir.getBiobanks():
+			if 'latitude' in biobank and not re.search('^\s*$', biobank['latitude']) and 'longitude' in biobank and not re.search('^\s*$', biobank['longitude']):
+				if re.search ('^-?\d+\.\d*$', biobank['latitude']) and re.search ('^-?\d+\.\d*$', biobank['longitude']):
+					if geoCodingEnabled:
+						print("Checking reverse geocoding for " + biobank['latitude'] + ", " + biobank['longitude'])
+						location = geolocator.reverse(biobank['latitude'] + ", " + biobank['longitude'])
+						country_code = location.raw['address']['country_code']
+						if (biobank['country']['id'] != "IARC" and country_code.upper() != biobank['country']['id']):
+							warning = Warning("", dir.getBiobankNN(biobank['id']), WarningLevel.WARNING, "Geolocation of the biobank is likely outside of its country " + biobank['id'] + " seems to be in " + country_code.upper())
+							warnings.append(warning)
+				else:
+					if not re.search ('^-?\d+\.\d*$', biobank['latitude']):
+						warning = Warning("", dir.getBiobankNN(biobank['id']), WarningLevel.ERROR, "Invalid biobank latitude for " + biobank['id'] + ": " + biobank['latitude'])
+						warnings.append(warning)
+					if not re.search ('^-?\d+\.\d*$', biobank['longitude']):
+						warning = Warning("", dir.getBiobankNN(biobank['id']), WarningLevel.ERROR, "Invalid biobank longitude for " + biobank['id'] + ": " + biobank['longitude'])
+						warnings.append(warning)
+			else:
+				warning = Warning("", dir.getBiobankNN(biobank['id']), WarningLevel.INFO, "Missing geographical coordinates for " + biobank['id'])
+				warnings.append(warning)
+
+
+		return warnings
