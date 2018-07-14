1b209501fa0282536c090b9a6aa41f94c9e164f3
Petr Holub <hopet@ics.muni.cz>
2018-07-14 09:05:15 +0200

    updates

--- checks/ContactFields.py
+++ checks/ContactFields.py
@@ -8,6 +8,7 @@ from customwarnings import DataCheckWarningLevel,DataCheckWarning
 class CheckContactFields(IPlugin):
 	def check(self, dir):
 		warnings = []
+		ValidateEmails = True
 		for contact in dir.getContacts():
 			if(not 'first_name' in contact or re.search('^\s*$', contact['first_name'])):
 				warning = DataCheckWarning("", dir.getContactNN(contact['id']), DataCheckWarningLevel.WARNING, "Missing first name for contact " + contact['id'])
@@ -21,10 +22,18 @@ class CheckContactFields(IPlugin):
 			elif(not validate_email(contact['email'])):
 				warning = DataCheckWarning("", dir.getContactNN(contact['id']), DataCheckWarningLevel.WARNING, "Email for contact " + contact['id'] + " is invalid " + contact['email'])
 				warnings.append(warning)
-			# This is pretty dramatic test and should be used sparingly
-			#if(not validate_email(contact['email'],verify=True)):
-				#warning = DataCheckWarning("", dir.getContactNN(contact['id']), DataCheckWarningLevel.WARNING, "Email for contact " + contact['id'] + " is invalid")
-				#warnings.append(warning)
+			else:
+				# This is pretty dramatic test and should be used sparingly
+				if ValidateEmails:
+					print("Validating email " + contact['email'], end=' ')
+					# XXX: does not work in most cases
+					#if(not validate_email(contact['email'],verify=True)):
+					if(not validate_email(contact['email'],check_mx=True)):
+						print(" -> failed")
+						warning = DataCheckWarning("", dir.getContactNN(contact['id']), DataCheckWarningLevel.WARNING, "Email for contact " + contact['id'] + " seems to be unreachable because of missing DNS MX record")
+						warnings.append(warning)
+					else:
+						print(" -> OK")
 			if(not 'phone' in contact or re.search('^\s*$', contact['phone'])):
 				warning = DataCheckWarning("", dir.getContactNN(contact['id']), DataCheckWarningLevel.WARNING, "Missing phone for contact " + contact['id'])
 				warnings.append(warning)
