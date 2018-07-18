7ba3899b934adf451d1f9f417b05908edf0ff0f0
Petr Holub <hopet@ics.muni.cz>
2018-07-18 11:42:39 +0200

    moved access policies to a separate check

---
+++ checks/AccessPolicies.py
@@ -0,0 +1,30 @@
+import re
+import urllib.request
+
+from yapsy.IPlugin import IPlugin
+from customwarnings import DataCheckWarningLevel,DataCheckWarning
+
+class AccessPolicies(IPlugin):
+	def check(self, dir):
+		warnings = []
+		for biobank in dir.getBiobanks():
+			if((not 'collaboration_commercial' in biobank or biobank['collaboration_commercial'] == False) and
+					(not 'collaboration_non_for_profit' in biobank or biobank['collaboration_non_for_profit'] == False)):
+				warning = DataCheckWarning(self.__class__.__name__, "", dir.getBiobankNN(biobank['id']), DataCheckWarningLevel.INFO, biobank['id'], "Biobank is available neither for commercial nor for non-for-profit collaboration")
+				warnings.append(warning)
+
+		for collection in dir.getCollections():
+
+			if((not 'data_access_fee' in collection or collection['data_access_fee'] == False) and 
+					(not 'data_access_joint_project' in collection or collection['data_access_joint_project'] == False) and 
+					(not 'data_access_uri' in collection or re.search('^\s*$', collection['data_access_uri']))):
+				warning = DataCheckWarning(self.__class__.__name__, "", dir.getCollectionNN(collection['id']), DataCheckWarningLevel.ERROR, collection['id'], "No data access mode enabled and no data access policy URI provided for collection")
+				warnings.append(warning)
+
+			if((not 'sample_access_fee' in collection or collection['sample_access_fee'] == False) and 
+					(not 'sample_access_joint_project' in collection or collection['sample_access_joint_project'] == False) and 
+					(not 'sample_access_uri' in collection or re.search('^\s*$', collection['sample_access_uri']))):
+				warning = DataCheckWarning(self.__class__.__name__, "", dir.getCollectionNN(collection['id']), DataCheckWarningLevel.ERROR, collection['id'], "No sample access mode enabled and no sample access policy URI provided for collection")
+				warnings.append(warning)
+
+		return warnings
