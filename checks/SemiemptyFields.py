11c171f88e3c103790003603b6ab7a342c32bca8
Petr Holub <hopet@ics.muni.cz>
2018-07-26 08:58:03 +0200

    updates

--- checks/SemiemptyFields.py
+++ checks/SemiemptyFields.py
@@ -20,7 +20,7 @@ class SemiemptyFields(IPlugin):
 				warning = DataCheckWarning(self.__class__.__name__, "", dir.getBiobankNN(biobank['id']), DataCheckWarningLevel.ERROR, biobank['id'], "Missing description for biobank")
 				warnings.append(warning)
 			if 'description' in biobank and descriptionTooShort(biobank['description']):
-				warning = DataCheckWarning(self.__class__.__name__, "", dir.getBiobankNN(biobank['id']), DataCheckWarningLevel.ERROR, biobank['id'], "Suspiciously short description for biobank (< " + str(minDescWords) + " words)")
+				warning = DataCheckWarning(self.__class__.__name__, "", dir.getBiobankNN(biobank['id']), DataCheckWarningLevel.WARNING, biobank['id'], "Suspiciously short description for biobank (< " + str(minDescWords) + " words)")
 				warnings.append(warning)
 			if not 'name' in biobank or re.search('^\s*$', biobank['name']) or re.search('^\s*N/?A\s*$', biobank['name']):
 				warning = DataCheckWarning(self.__class__.__name__, "", dir.getBiobankNN(biobank['id']), DataCheckWarningLevel.ERROR, biobank['id'], "Missing name for biobank")
@@ -28,10 +28,10 @@ class SemiemptyFields(IPlugin):
 
 		for collection in dir.getCollections():
 			if not 'description' in collection or re.search('^\s*$', collection['description']) or re.search('^\s*N/?A\s*$', collection['description']):
-				warning = DataCheckWarning(self.__class__.__name__, "", dir.getCollectionNN(collection['id']), DataCheckWarningLevel.ERROR, collection['id'], "Missing description for collection")
+				warning = DataCheckWarning(self.__class__.__name__, "", dir.getCollectionNN(collection['id']), DataCheckWarningLevel.WARNING, collection['id'], "Missing description for collection")
 				warnings.append(warning)
 			if 'description' in collection and descriptionTooShort(collection['description']):
-				warning = DataCheckWarning(self.__class__.__name__, "", dir.getCollectionNN(collection['id']), DataCheckWarningLevel.ERROR, collection['id'], "Suspiciously short description for collection (< " + str(minDescWords) + " words)")
+				warning = DataCheckWarning(self.__class__.__name__, "", dir.getCollectionNN(collection['id']), DataCheckWarningLevel.WARNING, collection['id'], "Suspiciously short description for collection (< " + str(minDescWords) + " words)")
 				warnings.append(warning)
 			if not 'name' in collection or re.search('^\s*$', collection['name']) or re.search('^\s*N/?A\s*$', collection['name']):
 				warning = DataCheckWarning(self.__class__.__name__, "", dir.getCollectionNN(collection['id']), DataCheckWarningLevel.ERROR, collection['id'], "Missing name for collection")
