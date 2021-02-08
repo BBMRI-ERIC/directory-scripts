892d5ef37827a904a807d64c0bc2c703ed08b9a0
Petr Holub <hopet@ics.muni.cz>
2021-02-08 15:10:43 +0100

    updated cancer counts for pediatrics

--- mission-cancer-exporter.py
+++ mission-cancer-exporter.py
@@ -79,6 +79,29 @@ cancerOnlyCollectionSamplesExplicit = 0
 cancerOnlyCollectionDonorsExplicit = 0
 cancerOnlyCollectionSamplesIncOoM = 0
 
+pediatricCancerExistingDiagnosed = []
+pediatricCancerOnlyExistingDiagnosed = []
+pediatricCancerBiobanksExistingDiagnosed = set()
+pediatricCancerOnlyBiobanksExistingDiagnosed = set()
+pediatricCancerBiobanks = set()
+pediatricCancerCollectionSamplesExplicit = 0
+pediatricCancerCollectionDonorsExplicit = 0
+pediatricCancerCollectionSamplesIncOoM = 0
+pediatricCancerOnlyCollectionSamplesExplicit = 0
+pediatricCancerOnlyCollectionDonorsExplicit = 0
+pediatricCancerOnlyCollectionSamplesIncOoM = 0
+pediatricOnlyCancerExistingDiagnosed = []
+pediatricOnlyCancerOnlyExistingDiagnosed = []
+pediatricOnlyCancerBiobanksExistingDiagnosed = set()
+pediatricOnlyCancerOnlyBiobanksExistingDiagnosed = set()
+pediatricOnlyCancerBiobanks = set()
+pediatricOnlyCancerCollectionSamplesExplicit = 0
+pediatricOnlyCancerCollectionDonorsExplicit = 0
+pediatricOnlyCancerCollectionSamplesIncOoM = 0
+pediatricOnlyCancerOnlyCollectionSamplesExplicit = 0
+pediatricOnlyCancerOnlyCollectionDonorsExplicit = 0
+pediatricOnlyCancerOnlyCollectionSamplesIncOoM = 0
+
 for collection in dir.getCollections():
     log.debug("Analyzing collection " + collection['id'])
     biobankId = dir.getCollectionBiobank(collection['id'])
@@ -172,31 +195,115 @@ for collection in dir.getCollections():
             else:
                 log.warn("Collection %s has invalid ORPHA code %s" % (d))
 
+    pediatric = False
+    pediatricOnly = False
+
+    age_unit = None
+    if 'age_unit' in collection:
+        age_units = collection['age_unit']
+        if len(age_units) > 1:
+            log.warn("Ambiguous age units provided for %s: %s"%(collection['id'],age_units))
+        if len(age_units) < 1:
+            log.warn("Age units missing for %s"%(collection['id']))
+        else:
+            age_unit = age_units[0]['id']
+
+    age_max = 18
+    if age_unit == "MONTH":
+        age_max = age_max*12
+    elif age_unit == "WEEK":
+        age_max = age_max*52.1775
+    elif age_unit == "DAY":
+        age_max = age_max*365.25
+
+    if 'age_high' in collection and collection['age_high'] == 0:
+        if 'age_low' in collection and collection['age_low'] < 0:
+            pediatric = True
+            pediatricOnly = True
+            log.debug("Prenatal collection detected: %s (%s), age range: %d-%d, diags: %s"%(collection['id'], collection['name'], collection.get('age_low'), collection.get('age_high'), diags + diag_ranges))
+        else:
+            log.warn("Age range mismatch detected for collection: %s (%s), age range: %d-%d"%(collection['id'], collection['name'], collection.get('age_low'), collection.get('age_high')))
+    elif 'age_high' in collection and collection['age_high'] < 0:
+        log.warn("Age range mismatch detected for collection: %s (%s), age range: %d-%d"%(collection['id'], collection['name'], collection.get('age_low'), collection.get('age_high')))
+    else:
+        if 'age_low' in collection and 'age_high' in collection and collection['age_low'] > collection['age_high']:
+            log.warn("Age range mismatch detected for collection: %s (%s), age range: %d-%d"%(collection['id'], collection['name'], collection.get('age_low'), collection.get('age_high')))
+        else:
+            if (('age_low' in collection and collection['age_low'] < age_max ) or ('age_high' in collection and collection['age_high'] < age_max)):
+                pediatric = True
+            if 'age_high' in collection and collection['age_high'] < age_max:
+                pediatricOnly = True
+                log.debug("Pediatric-only collection detected: %s, age range: %d-%d, diags: %s"%(collection['id'], collection.get('age_low'), collection.get('age_high'), diags + diag_ranges))
+
     if cancer_diag:
-        log.info("Collection " + collection['id'] + " has cancer cases")
+        log.info(f"Cancer collection detected: {collection['id']}")
         cancerExistingDiagnosed.append(collection)
         cancerBiobanksExistingDiagnosed.add(biobankId)
         cancerBiobanks.add(biobankId)
+        if pediatric:
+            log.info(f"Pediatric cancer collection detected: {collection['id']}, age range: {collection.get('age_low')}-{collection.get('age_high')}, diags: {diags + diag_ranges}")
+            pediatricCancerExistingDiagnosed.append(collection)
+            pediatricCancerBiobanksExistingDiagnosed.add(biobankId)
+            pediatricCancerBiobanks.add(biobankId)
+        if pediatricOnly:
+            log.info(f"Pediatric-only cancer collection detected: {collection['id']}, age range: {collection.get('age_low')}-{collection.get('age_high')}, diags: {diags + diag_ranges}")
+            pediatricOnlyCancerExistingDiagnosed.append(collection)
+            pediatricOnlyCancerBiobanksExistingDiagnosed.add(biobankId)
+            pediatricOnlyCancerBiobanks.add(biobankId)
         if not non_cancer:
-            log.info("Collection %s has cancer cases only" % (collection['id']))
+            log.info(f"Cancer-only collection detected:  {collection['id']}")
             cancerOnlyExistingDiagnosed.append(collection)
             cancerOnlyBiobanksExistingDiagnosed.add(biobankId)
+            if pediatric:
+                pediatricCancerOnlyExistingDiagnosed.append(collection)
+                pediatricCancerOnlyBiobanksExistingDiagnosed.add(biobankId)
+            if pediatricOnly:
+                pediatricOnlyCancerOnlyExistingDiagnosed.append(collection)
+                pediatricOnlyCancerOnlyBiobanksExistingDiagnosed.add(biobankId)
         else:
-            log.info("Collection %s has mixture of cancer and non-cancer cases" % (collection['id']))
+            log.info(f"Mixed cancer/non-cancer collection detected: {collection['id']}")
         if 'size' in collection and isinstance(collection['size'], int):
             cancerCollectionSamplesExplicit += collection['size']
             cancerCollectionSamplesIncOoM += collection['size']
+            if pediatric:
+                pediatricCancerCollectionSamplesExplicit += collection['size']
+                pediatricCancerCollectionSamplesIncOoM += collection['size']
+            if pediatricOnly:
+                pediatricOnlyCancerCollectionSamplesExplicit += collection['size']
+                pediatricOnlyCancerCollectionSamplesIncOoM += collection['size']
             if not non_cancer:
                 cancerOnlyCollectionSamplesExplicit += collection['size']
                 cancerOnlyCollectionSamplesIncOoM += collection['size']
+                if pediatric:
+                    pediatricCancerOnlyCollectionSamplesExplicit += collection['size']
+                    pediatricCancerOnlyCollectionSamplesIncOoM += collection['size']
+                if pediatricOnly:
+                    pediatricOnlyCancerOnlyCollectionSamplesExplicit += collection['size']
+                    pediatricOnlyCancerOnlyCollectionSamplesIncOoM += collection['size']
         else:
             cancerCollectionSamplesIncOoM += 10 ** OoM
+            if pediatric:
+                pediatricCancerCollectionSamplesIncOoM += 10 ** OoM
+            if pediatricOnly:
+                pediatricOnlyCancerCollectionSamplesIncOoM += 10 ** OoM
             if not non_cancer:
                 cancerOnlyCollectionSamplesIncOoM += 10 ** OoM
+                if pediatric:
+                    pediatricCancerOnlyCollectionSamplesIncOoM += 10 ** OoM
+                if pediatricOnly:
+                    pediatricOnlyCancerOnlyCollectionSamplesIncOoM += 10 ** OoM
         if 'number_of_donors' in collection and isinstance(collection['number_of_donors'], int):
             cancerCollectionDonorsExplicit += collection['number_of_donors']
+            if pediatric:
+                pediatricCancerCollectionDonorsExplicit += collection['number_of_donors']
+            if pediatricOnly:
+                pediatricOnlyCancerCollectionDonorsExplicit += collection['number_of_donors']
             if not non_cancer:
                 cancerOnlyCollectionDonorsExplicit += collection['number_of_donors']
+                if pediatric:
+                    pediatricCancerOnlyCollectionDonorsExplicit += collection['number_of_donors']
+                if pediatricOnly:
+                    pediatricOnlyCancerOnlyCollectionDonorsExplicit += collection['number_of_donors']
 
 pd_cancerExistingDiagnosed = pd.DataFrame(cancerExistingDiagnosed)
 pd_cancerExistingControls = pd.DataFrame(cancerExistingControls)
@@ -219,28 +326,59 @@ if not args.nostdout:
     printCollectionStdout(cancerProspective, "Cancer Prospective")
     print("\n\n")
     print("Totals:")
-    print("- total number of cancer-relevant biobanks: %d" % (len(cancerBiobanks)))
-    print("- total number of cancer-relevant collections with existing samples: %d in %d biobanks" % (
+    print("- total of cancer-relevant biobanks: %d" % (len(cancerBiobanks)))
+    print("- total of cancer-relevant collections with existing samples: %d in %d biobanks" % (
     len(cancerExistingDiagnosed), len(cancerBiobanksExistingDiagnosed)))
-    print("- total number of exclusive cancer collections with existing samples: %d in %d biobanks" % (
+    print("- total of cancer-only collections with existing samples: %d in %d biobanks" % (
     len(cancerOnlyExistingDiagnosed), len(cancerOnlyBiobanksExistingDiagnosed)))
-    print("- total number of cancer-relevant collections with control samples: %d in %d biobanks" % (
+    print("- total of cancer-relevant collections with control samples: %d in %d biobanks" % (
     len(cancerExistingControls), len(cancerBiobanksExistingControls)))
-    print("- total number of cancer-relevant prospective collections: %d in %d biobanks" % (
+    print("- total of cancer-relevant prospective collections: %d in %d biobanks" % (
     len(cancerProspective), len(cancerBiobanksProspective)))
     print("Estimated totals:")
-    print("- total number of samples advertised explicitly in cancer-only collections: %d" % (
-        cancerOnlyCollectionSamplesExplicit))
-    print("- total number of samples advertised explicitly in cancer-relevant collections: %d" % (
-        cancerCollectionSamplesExplicit))
-    print("- total number of donors advertised explicitly in cancer-only collections: %d" % (
-        cancerOnlyCollectionDonorsExplicit))
-    print("- total number of donors advertised explicitly in cancer-relevant collections: %d" % (
-        cancerCollectionDonorsExplicit))
-    print("- total number of samples advertised in cancer-only collections including OoM estimates: %d" % (
+    print("- total of samples/donors advertised explicitly in cancer-only collections: %d / %d" % (
+        cancerOnlyCollectionSamplesExplicit, cancerOnlyCollectionDonorsExplicit))
+    print("- total of samples advertised in cancer-only collections including OoM estimates: %d" % (
         cancerOnlyCollectionSamplesIncOoM))
-    print("- total number of samples advertised in cancer-relevant collections including OoM estimates: %d" % (
+    print("- total of samples/donors advertised explicitly in cancer-relevant collections: %d / %d" % (
+        cancerCollectionSamplesExplicit, cancerCollectionDonorsExplicit))
+    print("- total of samples advertised in cancer-relevant collections including OoM estimates: %d" % (
         cancerCollectionSamplesIncOoM))
+    print("\n\n")
+    printCollectionStdout(pediatricCancerExistingDiagnosed, "Pediatric Cancer Diagnosed")
+    printCollectionStdout(pediatricOnlyCancerExistingDiagnosed, "Pediatric Only Cancer Diagnosed")
+    print("\n\n")
+    print("Biobanks/collections totals:")
+    print("- total of pediatric cancer-relevant biobanks: %d" % (len(pediatricCancerBiobanks)))
+    print("- total of pediatric cancer-relevant collections with existing samples: %d in %d biobanks" % (
+    len(pediatricCancerExistingDiagnosed), len(pediatricCancerBiobanksExistingDiagnosed)))
+    print("- total of pediatric cancer-only collections with existing samples: %d in %d biobanks" % (
+    len(pediatricCancerOnlyExistingDiagnosed), len(pediatricCancerOnlyBiobanksExistingDiagnosed)))
+    print("- total of pediatric-only cancer-relevant biobanks: %d" % (len(pediatricOnlyCancerBiobanks)))
+    print("- total of pediatric-only cancer-relevant collections with existing samples: %d in %d biobanks" % (
+    len(pediatricOnlyCancerExistingDiagnosed), len(pediatricOnlyCancerBiobanksExistingDiagnosed)))
+    print("- total of pediatric-only cancer-only collections with existing samples: %d in %d biobanks" % (
+    len(pediatricOnlyCancerOnlyExistingDiagnosed), len(pediatricOnlyCancerOnlyBiobanksExistingDiagnosed)))
+    print("\n\n")
+    print("Estimated sample totals:")
+    print("- total of samples/donors advertised explicitly in pediatric-only cancer-only collections: %d / %d" % (
+        pediatricOnlyCancerOnlyCollectionSamplesExplicit, pediatricOnlyCancerOnlyCollectionDonorsExplicit))
+    print("- total of samples advertised in pediatric-only cancer-only collections including OoM estimates: %d" % (
+        pediatricOnlyCancerOnlyCollectionSamplesIncOoM))
+    print("- total of samples/donors advertised explicitly in pediatric-only cancer-relevant collections: %d / %d" % (
+        pediatricOnlyCancerCollectionSamplesExplicit, pediatricOnlyCancerCollectionDonorsExplicit))
+    print("- total of samples advertised in pediatric-only cancer-relevant collections including OoM estimates: %d" % (
+        pediatricOnlyCancerCollectionSamplesIncOoM))
+    print("\n")
+    print("- total of samples/donors advertised explicitly in pediatric cancer-only collections: %d / %d" % (
+        pediatricCancerOnlyCollectionSamplesExplicit, pediatricCancerOnlyCollectionDonorsExplicit))
+    print("- total of samples advertised in pediatric cancer-only collections including OoM estimates: %d" % (
+        pediatricCancerOnlyCollectionSamplesIncOoM))
+    print("- total of samples/donors advertised explicitly in pediatric cancer-relevant collections: %d / %d" % (
+        pediatricCancerCollectionSamplesExplicit, pediatricCancerCollectionDonorsExplicit))
+    print("- total of samples advertised in pediatric cancer-relevant collections including OoM estimates: %d" % (
+        pediatricCancerCollectionSamplesIncOoM))
+
 
 if args.outputXLSX is not None:
     log.info("Outputting warnings in Excel file " + args.outputXLSX[0])
