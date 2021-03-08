aeddade8bd86e94baab4693ec981864da566f4d4
Petr Holub <hopet@ics.muni.cz>
2021-03-08 11:35:00 +0100

    removed benign neoplasms from cancer ranges

--- icd10codeshelper.py
+++ icd10codeshelper.py
@@ -8,7 +8,7 @@ import re
 import roman
 
 cancer_diag_ranges = ['C00-D49', 'C00-C97', 'C00-C75', 'C00-C14', 'C15-C26', 'C30-C39', 'C40-C41', 'C43-C44', 'C45-C49',
-                      'C51-C58', 'C60-C63', 'C64-C68', 'C69-C72', 'C73-C75', 'C76-C80', 'C81-C96', 'D00-D09', 'D10-D36',
+                      'C51-C58', 'C60-C63', 'C64-C68', 'C69-C72', 'C73-C75', 'C76-C80', 'C81-C96', 'D00-D09', 
                       'D37-D48', 'C50-C50', 'C97-C97']
 
 cancer_chapters_roman = list(map(roman.toRoman, range(1, 23)))  # chapter 22 added in 2020
@@ -22,7 +22,7 @@ class ICD10CodesHelper:
       m = re.search(r'^(?P<block>[A-Z])(?P<code>\d{1,2})(\.(?P<subcode>\d+))?$', code)
       if m:
          log.debug("ICD-10 block detected: %s, code: %s, subcode %s" % (m.group('block'), m.group('code'), m.group('subcode')))
-         if m.group('block') == 'C' or (m.group('block') == 'D' and int(m.group('code')) <= 48):
+         if m.group('block') == 'C' or (m.group('block') == 'D' and (0 <= int(m.group('code')) <= 9 or 37 <= int(m.group('code')) <= 48)):
             return True
          else:
             return False
