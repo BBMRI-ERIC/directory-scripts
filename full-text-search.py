4f58fab2483b286fa5fee06f35a5a2b4e84afd23
Petr Holub <hopet@ics.muni.cz>
2021-04-07 16:59:20 +0200

    index can be reused now and not rebuilt with every run

--- full-text-search.py
+++ full-text-search.py
@@ -13,13 +13,13 @@ import os.path
 
 from directory import Directory
 
-from whoosh.index import create_in
+from whoosh.index import create_in, open_dir
 from whoosh.fields import *
 from whoosh.analysis import *
 from whoosh.query import *
 from whoosh.support.charset import accent_map
 
-cachesList = ['directory', 'emails', 'geocoding', 'URLs']
+cachesList = ['directory', 'index']
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -50,48 +50,46 @@ else:
 
 # Main code
 
-dir = Directory(purgeCaches=args.purgeCaches, debug=args.debug, pp=pp)
-
-log.info('Total biobanks: ' + str(dir.getBiobanksCount()))
-log.info('Total collections: ' + str(dir.getCollectionsCount()))
 
 indexdir = "indexdir"
 
-if not os.path.exists(indexdir):
-    os.makedirs(indexdir)
 
-my_ana = StemmingAnalyzer() | CharsetFilter(accent_map)
-my_id_ana = RegexTokenizer(expression=re.compile('[^ ]+')) | LowercaseFilter() | TeeFilter(PassFilter(), IntraWordFilter(delims=u':',splitnums=False) | StopFilter(stoplist=frozenset(['bbmri-eric', 'id', 'contactid', 'networkid', 'collection']))) # | LoggingFilter()
-schema = Schema(id=TEXT(stored=True,analyzer=my_id_ana), type=STORED, name=TEXT(stored=True,analyzer=my_ana), acronym=ID, description=TEXT(analyzer=my_ana), address=TEXT(analyzer=my_ana), phone=TEXT, email=TEXT, juridical_person=TEXT(analyzer=my_ana), bioresource_reference=TEXT, head_name=TEXT(analyzer=my_ana))
-ix = create_in(indexdir, schema)
-writer = ix.writer()
+if 'index' in args.purgeCaches or not os.path.exists(indexdir):
+	dir = Directory(purgeCaches=args.purgeCaches, debug=args.debug, pp=pp)
 
-def getFullName(entity):
-	return " ".join(filter(None,[entity.get('head_title_before_name'), entity.get('head_firstname'), entity.get('head_lastname'), entity.get('head_title_after_name')]))
+	log.info('Total biobanks: ' + str(dir.getBiobanksCount()))
+	log.info('Total collections: ' + str(dir.getCollectionsCount()))
 
-for collection in dir.getCollections():
-	log.debug("Analyzing collection " + collection['id'])
-	biobankId = dir.getCollectionBiobank(collection['id'])
-	biobank = dir.getBiobankById(biobankId)
-	writer.add_document(id=collection['id'], type=u"COLLECTION", name=collection.get('name'), description=collection.get('description'), acronym=collection.get('acronym'), bioresource_reference=collection.get('bioresource_reference'), head_name=getFullName(collection))
+	if not os.path.exists(indexdir):
+	    os.makedirs(indexdir)
 
-for biobank in dir.getBiobanks():
-	log.debug("Analyzing biobank " + biobank['id'])
-	writer.add_document(id=biobank['id'], type=u"BIOBANK", name=biobank.get('name'), description=biobank.get('description'), acronym=biobank.get('acronym'), juridical_person=biobank.get('juridical_person'), bioresource_reference=biobank.get('bioresource_reference'), head_name=getFullName(biobank))
+	my_ana = StemmingAnalyzer() | CharsetFilter(accent_map)
+	my_id_ana = RegexTokenizer(expression=re.compile('[^ ]+')) | LowercaseFilter() | TeeFilter(PassFilter(), IntraWordFilter(delims=u':',splitnums=False) | StopFilter(stoplist=frozenset(['bbmri-eric', 'id', 'contactid', 'networkid', 'collection']))) # | LoggingFilter()
+	schema = Schema(id=TEXT(stored=True,analyzer=my_id_ana), type=STORED, name=TEXT(stored=True,analyzer=my_ana), acronym=ID, description=TEXT(analyzer=my_ana), address=TEXT(analyzer=my_ana), phone=TEXT, email=TEXT, juridical_person=TEXT(analyzer=my_ana), bioresource_reference=TEXT, head_name=TEXT(analyzer=my_ana))
+	ix = create_in(indexdir, schema)
+	writer = ix.writer()
 
-for contact in dir.getContacts():
-	log.debug("Analyzing contact " + contact['id'])
-	writer.add_document(id=contact['id'], type=u"CONTACT", name=" ".join(filter(None,[contact.get('title_before_name'), contact.get('first_name'), contact.get('last_name'), contact.get('title_after_name')])), phone=contact.get('phone'), email=contact.get('email'), address=", ".join(filter(None,[contact.get('address'),contact.get('city'),contact.get('zip')])))
+	def getFullName(entity):
+		return " ".join(filter(None,[entity.get('head_title_before_name'), entity.get('head_firstname'), entity.get('head_lastname'), entity.get('head_title_after_name')]))
 
-writer.commit()
-
-def printCollectionStdout(collectionList : List, headerStr : str):
-	print(headerStr + " - " + str(len(collectionList)) + " collections")
-	for collection in collectionList:
+	for collection in dir.getCollections():
+		log.debug("Analyzing collection " + collection['id'])
 		biobankId = dir.getCollectionBiobank(collection['id'])
 		biobank = dir.getBiobankById(biobankId)
-		print("   Collection: " + collection['id'] + " - " + collection['name'] + ". Parent biobank: " +  biobankId + " - " + biobank['name'])
+		writer.add_document(id=collection['id'], type=u"COLLECTION", name=collection.get('name'), description=collection.get('description'), acronym=collection.get('acronym'), bioresource_reference=collection.get('bioresource_reference'), head_name=getFullName(collection))
 
+	for biobank in dir.getBiobanks():
+		log.debug("Analyzing biobank " + biobank['id'])
+		writer.add_document(id=biobank['id'], type=u"BIOBANK", name=biobank.get('name'), description=biobank.get('description'), acronym=biobank.get('acronym'), juridical_person=biobank.get('juridical_person'), bioresource_reference=biobank.get('bioresource_reference'), head_name=getFullName(biobank))
+
+	for contact in dir.getContacts():
+		log.debug("Analyzing contact " + contact['id'])
+		writer.add_document(id=contact['id'], type=u"CONTACT", name=" ".join(filter(None,[contact.get('title_before_name'), contact.get('first_name'), contact.get('last_name'), contact.get('title_after_name')])), phone=contact.get('phone'), email=contact.get('email'), address=", ".join(filter(None,[contact.get('address'),contact.get('city'),contact.get('zip')])))
+
+	writer.commit()
+
+else:
+	ix = open_dir(indexdir)
 
 matchingCollections = {}
 matchingBiobanks = {}
