516f3ef435b2eb8fbc09469fcad68cb47c347191
Petr Holub <hopet@ics.muni.cz>
2021-05-31 15:09:20 +0200

    Fixed FI contact information

--- nncontacts.py
+++ nncontacts.py
@@ -11,13 +11,13 @@ class NNContacts:
 			'DE' : 'michael.hummel@charite.de, caecilia.engels@charite.de, cornelia.specht@charite.de',
 			'EE' : 'andres.metspalu@ut.ee, kristjan.metsalu@ut.ee',
 			'EU' : 'petr.holub@bbmri-eric.eu, e.van.enckevort@rug.nl',
-			'FI' : 'marco.hautalahti@finbb.fi, paivi.luotola@finbb.fi, Niina.Eklund@thl.fi',
+			'FI' : 'marco.hautalahti@finbb.fi, pauli.wihuri@finbb.fi',
 			'FR' : 'soraya.aakki@inserm.fr, michael.hisbergues@inserm.fr',
 			'GR' : 's.kolyva@pasteur.gr, thanos@bioacademy.gr, koumakis@ics.forth.gr',
 			'IT' : 'marialuisa.lavitrano@unimib.it, luciano.milanesi@itb.cnr.it, barbara.parodi@hsanmartino.it, elena.bravo@iss.it, matteo.gnocchi@itb.cnr.it, marco.moscatelli@itb.cnr.it',
 			'LV' : 'klovins@biomed.lu.lv, vita@biomed.lu.lv, inese.polaka@rtu.lv, linda.zaharenko@biomed.lu.lv',
 			'MT' : 'joanna.vella@um.edu.mt, alex.felice@um.edu.mt',
-			'NL' : 'e.j.van.enckevort@rug.nl, g.meijer@nki.nl, a.vanderlugt@erasmusmc.nl, m.a.swertz@gmail.com;',
+			'NL' : 'e.j.van.enckevort@rug.nl, m.a.swertz@gmail.com, tieneke.schaaij-visser@lygature.org',
 			'NO' : 'vegard.marschhauser@ntnu.no, kristian.hveem@ntnu.no',
 			'PL' : 'dominik.strapagiel@biol.uni.lodz.pl, blazej.marciniak@biol.uni.lodz.pl, andrzej.strug@gumed.edu.pl, aklis@bee2code.com',
 			'SE' : 'tobias.sjoblom@igp.uu.se',
@@ -26,3 +26,4 @@ class NNContacts:
 			'IARC' : 'kozlakidisz@iarc.fr',
 			}
 
+			# 'NL' : 'e.j.van.enckevort@rug.nl, g.meijer@nki.nl, a.vanderlugt@erasmusmc.nl, m.a.swertz@gmail.com;',
