f36c57f6f3bc5b0d460b7aec0087eab4f44b0099
Petr Holub <hopet@ics.muni.cz>
2024-04-30 11:16:46 +0200

    update of all-exporter

--- pddfutils.py
+++ pddfutils.py
@@ -7,6 +7,7 @@ from typing import List
 import pandas as pd
 
 def extractContactDetails (df : pd.DataFrame):
+    assert isinstance(df, pd.DataFrame)
     if 'contact' in df:
         df['contact_email'] = df['contact'].apply(lambda c: c['email'] if type(c) is dict and 'email' in c else "")
         df['contact_name'] = df['contact'].apply(lambda c: " ".join([x for x in [c.get('first_name'), c.get('last_name')] if x]) if type(c) is dict else "")
@@ -14,6 +15,7 @@ def extractContactDetails (df : pd.DataFrame):
         for e in ['address', 'zip', 'city', 'country', 'phone']:
             # country is a dict, hence the 'id' hack
             df['contact_'+e] = df['contact'].apply(lambda c: c[e]['id'] if (type (c) is dict and e in c and type(c[e]) is dict and 'id' in c[e]) else c[e] if (type (c) is dict and e in c) else "").apply(lambda c: c.replace("\n",", "))
+        del df['contact']
 
 def linearizeStructures (df : pd.DataFrame, rules : list):
     for (col, attr) in rules:
@@ -21,6 +23,7 @@ def linearizeStructures (df : pd.DataFrame, rules : list):
             df[col] = df[col].map(lambda v: ",".join(map(lambda x: x[attr] if type(x) is dict and attr in x else x, (v if type(v) is list else [v]))) if v and (type(v) is dict or type(v) is list) else "")
 
 def tidyCollectionDf (df : pd.DataFrame):
+    assert isinstance(df, pd.DataFrame)
     linearizeStructures(df, [('country','id'),('biobank','name'),('network','name'),('parent_collection','id')])
     for col in ('order_of_magnitude','order_of_magnitude_donors'):
         if col in df:
@@ -31,17 +34,15 @@ def tidyCollectionDf (df : pd.DataFrame):
     if 'diagnosis_available' in df:
         df['diagnosis_available'] = df['diagnosis_available'].map(lambda x: ",".join([re.sub('^urn:miriam:icd:','',e['id']) for e in x]) )
     extractContactDetails(df)
-    for c in ['contact']:
-        del df[c]
     df.sort_values(by=['country','id'],ascending=True,inplace=True)
 
 def tidyBiobankDf (df : pd.DataFrame):
+    assert isinstance(df, pd.DataFrame)
     linearizeStructures(df, [('country','id'), ('network','name'), ('covid19biobank','id'), ('capabilities','id'), ('quality','id')])
     extractContactDetails(df)
     for col in ['it_support_available', 'it_staff_size', 'is_available', 'his_available', 'partner_charter_signed', 'collections','contact']:
-        try:
+        if col in df:
             del df[col]
-        except KeyError:
-            pass
+    assert isinstance(df, pd.DataFrame)
     df.sort_values(by=['country','id'],ascending=True,inplace=True)
             
