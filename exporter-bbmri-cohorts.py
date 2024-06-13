2b0d1860b8086a410869228759dd5604a6581e88
EvaGarciaAlvarez <eva.garcia-alvarez@bbmri-eric.eu>
2024-06-13 10:13:41 +0100

    update exporter-bbmri-cohorts.py

--- exporter-bbmri-cohorts.py
+++ exporter-bbmri-cohorts.py
@@ -9,20 +9,13 @@ BBMRI-ERIC Directory Cohorts
 
 # External
 import pprint
-import re
 import argparse
-import json
-import configparser
-import geopy.geocoders
-from dms2dec.dms_convert import dms2dec
-import ssl
 import logging as log
-import smtplib
 import pandas as pd
-from flatten_json import flatten
 
 # Internal
 from directory import Directory
+#from dataCheck import WarningsContainer
 
 cachesList = ['directory', 'geocoding']
 
@@ -32,7 +25,7 @@ cachesList = ['directory', 'geocoding']
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--purge-all-caches', dest='purgeCaches', action='store_const', const=cachesList, help='disable all long remote checks (directory and geocoding)')
-parser.add_argument('-a', '--aggregator', dest='aggregator', type=str, default=['Network','Entity','Country'], help='Space-separated list of the aggregators used in stdout. Accepted values: Network Entity Country')
+parser.add_argument('-a', '--aggregator', dest='aggregator', type=str, default=['Network','Entity','Country','NrCollWithSampleDonorProvided','NrCollWithFactsProvided','nrSamplesFactTables'], help='Space-separated list of the aggregators used in stdout. Accepted values: Network Entity Country')
 parser.add_argument('-X', '--output-XLSX', dest='outputXLSX', default='bbmri_cohorts_stats.xlsx',
                     help='output of results into an XLSX with filename provided as parameter')
 parser.add_argument('-o', '--outName', dest='outName', default='bbmri-directory-5-0', help='Output file name')
@@ -93,49 +86,92 @@ for collection in dir.getCollections():
                     checkedBbsIdsCohortDNA.append(biobankId)
         
 
-df  = pd.DataFrame(columns = ['Network','Entity','Country'])
+df  = pd.DataFrame(columns = ['Network','Entity','Country','NrCollWithSampleDonorProvided','NrCollWithFactsProvided','nrSamplesFactTables'])
 df_bb  = pd.DataFrame(columns = ['Network','Entity','Country','Name','ID'])
 df_coll  = pd.DataFrame(columns = ['Network','Entity','Country','Name','ID'])
-
+df_collFactsSampleNumber  = pd.DataFrame(columns = ['Network','Entity','Country','Name','ID','NumberOfSamples'])
 
 for biobank_cohort in bbmri_cohort_bb:
-    df.loc[len(df)] = ['BBMRI_Cohort','Biobank',str(biobank_cohort['country']['id'])]
+    df.loc[len(df)] = ['BBMRI_Cohort','Biobank',str(biobank_cohort['country']['id']),'NA','NA',int(0)]
     df_bb.loc[len(df)] = ['BBMRI_Cohort','Biobank',str(biobank_cohort['country']['id']),str(biobank_cohort['name']),str(biobank_cohort['id'])]
     log.info('BBMRI_Cohort' + '\tBiobank\t' + str(biobank_cohort['country']['id']))
 
 for biobank_cohort_dna in bbmri_cohort_dna_bb:
-    df.loc[len(df)] = ['BBMRI_Cohort_DNA','Biobank',str(biobank_cohort_dna['country']['id'])]
+    df.loc[len(df)] = ['BBMRI_Cohort_DNA','Biobank',str(biobank_cohort_dna['country']['id']),'NA','NA',int(0)]
     df_bb.loc[len(df)] = ['BBMRI_Cohort_DNA','Biobank',str(biobank_cohort_dna['country']['id']),str(biobank_cohort_dna['name']),str(biobank_cohort_dna['id'])]
     log.info('BBMRI_Cohort_DNA' + '\tBiobank\t' + str(biobank_cohort_dna['country']['id']))
 
 for bbcoll_cohort in bbmri_cohort_bbcoll:
-    df.loc[len(df)] = ['BBMRI_Cohort','BiobankCollection',str(bbcoll_cohort['country']['id'])]
-    df_bb.loc[len(df)] = ['BBMRI_Cohort','BiobankCollection',str(bbcoll_cohort['country']['id']),str(bbcoll_cohort['name']),str(bbcoll_cohort['id'])]
+    df.loc[len(df)] = ['BBMRI_Cohort','BiobankWithCollectionInNetwork',str(bbcoll_cohort['country']['id']),'NA','NA',int(0)]
+    df_bb.loc[len(df)] = ['BBMRI_Cohort','BiobankWithCollectionInNetwork',str(bbcoll_cohort['country']['id']),str(bbcoll_cohort['name']),str(bbcoll_cohort['id'])]
     log.info('BBMRI_Cohort' + '\tCollection\t' + str(bbcoll_cohort['country']['id']))
 
 for bbcoll_cohort_dna in bbmri_cohort_dna_bbcoll:
-    df.loc[len(df)] = ['BBMRI_Cohort_DNA','BiobankCollection',str(bbcoll_cohort_dna['country']['id'])]
-    df_bb.loc[len(df)] = ['BBMRI_Cohort_DNA','BiobankCollection',str(bbcoll_cohort_dna['country']['id']),str(bbcoll_cohort_dna['name']),str(bbcoll_cohort_dna['id'])]
+    df.loc[len(df)] = ['BBMRI_Cohort_DNA','BiobankWithCollectionInNetwork',str(bbcoll_cohort_dna['country']['id']),'NA','NA',int(0)]
+    df_bb.loc[len(df)] = ['BBMRI_Cohort_DNA','BiobankWithCollectionInNetwork',str(bbcoll_cohort_dna['country']['id']),str(bbcoll_cohort_dna['name']),str(bbcoll_cohort_dna['id'])]
     log.info('BBMRI_Cohort_DNA' + '\tCollection\t' + str(bbcoll_cohort_dna['country']['id']))
 
+
 for coll_cohort in bbmri_cohort_coll:
-    df.loc[len(df)] = ['BBMRI_Cohort','Collection',str(coll_cohort['country']['id'])]
+    nrSampDonProv = 'N'
+    collsFactsSamples = 0
+    factsProvided = 'N'
     df_coll.loc[len(df)] = ['BBMRI_Cohort','Collection',str(coll_cohort['country']['id']),str(coll_cohort['name']),str(coll_cohort['id'])]
+    if 'size' in coll_cohort and 'number_of_donors' in coll_cohort:
+        if isinstance(coll_cohort['size'], int) and isinstance(coll_cohort['number_of_donors'], int):
+            nrSampDonProv = 'Y'
+
+    #Facts table
+    if coll_cohort['facts'] != []:
+        for fact in dir.getFacts():
+            if fact['collection']['id'] == coll_cohort['id']:
+                if 'number_of_samples' in fact:
+                    collsFactsSamples += fact['number_of_samples']
+        if collsFactsSamples > 0:
+            factsProvided = 'Y'
+            df_collFactsSampleNumber.loc[len(df)] = ['BBMRI_Cohort','Collection',str(coll_cohort['country']['id']),str(coll_cohort['name']),str(coll_cohort['id']),int(collsFactsSamples)]
     log.info('BBMRI_Cohort' + '\tCollection\t' + str(coll_cohort['country']['id']))
+    df.loc[len(df)] = ['BBMRI_Cohort','Collection',str(coll_cohort['country']['id']),str(nrSampDonProv),str(factsProvided),int(collsFactsSamples)]
+
 
 for coll_cohort_dna in bbmri_cohort_dna_coll:
-    df.loc[len(df)] = ['BBMRI_Cohort_DNA','Collection',str(coll_cohort_dna['country']['id'])]
+    nrSampDonProv = 'N'
+    collsFactsSamples = 0
+    factsProvided = 'N'
+
     df_coll.loc[len(df)] = ['BBMRI_Cohort_DNA','Collection',str(coll_cohort_dna['country']['id']),str(coll_cohort_dna['name']),str(coll_cohort_dna['id'])]
+    if 'size' in coll_cohort_dna and 'number_of_donors' in coll_cohort_dna:
+        if isinstance(coll_cohort_dna['size'], int) and isinstance(coll_cohort_dna['number_of_donors'], int):
+            nrSampDonProv = 'Y'
+
+    #Facts table
+    if coll_cohort_dna['facts'] != []:
+        for fact in dir.getFacts():
+            if fact['collection']['id'] == coll_cohort_dna['id']:
+                if 'number_of_samples' in fact:
+                    collsFactsSamples += fact['number_of_samples']
+        if collsFactsSamples > 0:
+            factsProvided = 'Y'
+            df_collFactsSampleNumber.loc[len(df)] = ['BBMRI_Cohort_DNA','Collection',str(coll_cohort_dna['country']['id']),str(coll_cohort_dna['name']),str(coll_cohort_dna['id']),int(collsFactsSamples)]
     log.info('BBMRI_Cohort_DNA' + '\tCollection\t' + str(coll_cohort_dna['country']['id']))
+    df.loc[len(df)] = ['BBMRI_Cohort_DNA','Collection',str(coll_cohort_dna['country']['id']),str(nrSampDonProv),str(factsProvided),int(collsFactsSamples)]
 
 
-def outputExcelBiobanksCollections(filename : str, dfBiobanks : pd.DataFrame, biobanksLabel : str, dfCollections : pd.DataFrame, collectionsLabel : str, dfStats : pd.DataFrame, statsLabel : str):
+def outputExcelBiobanksCollections(filename : str, dfBiobanks : pd.DataFrame, biobanksLabel : str, dfCollections : pd.DataFrame, collectionsLabel : str, dfStats : pd.DataFrame, statsLabel : str, numberSamplesFacts : pd.DataFrame, samplesFactsLabel : str):
     log.info("Outputting warnings in Excel file " + filename)
     writer = pd.ExcelWriter(filename, engine='xlsxwriter')
     dfBiobanks.to_excel(writer, sheet_name=biobanksLabel)
     dfCollections.to_excel(writer, sheet_name=collectionsLabel)
     dfStats.to_excel(writer, sheet_name=statsLabel)
+    numberSamplesFacts.to_excel(writer, sheet_name=samplesFactsLabel)
     writer.close()
 
-statsdf = df.groupby(aggregator).size().reset_index(name='Count')
-outputExcelBiobanksCollections(args.outputXLSX, df_bb, "Biobanks", df_coll, "Collections", statsdf, "Stats")
+
+countCountries = df.groupby(aggregator).size().reset_index(name='Count')
+
+columns_to_sum = ['nrSamplesFactTables','Count']
+columns_to_group_by = ['Network','Entity','Country','NrCollWithSampleDonorProvided','NrCollWithFactsProvided']
+
+statsdf = countCountries.groupby(columns_to_group_by, as_index=False).agg({col: 'sum' for col in columns_to_sum})
+
+outputExcelBiobanksCollections(args.outputXLSX, df_bb, "Biobanks", df_coll, "Collections", statsdf, "Stats", df_collFactsSampleNumber, "NumberOfSamplesFactTable")
